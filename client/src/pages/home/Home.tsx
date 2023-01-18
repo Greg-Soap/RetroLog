d3df7fcf0fbe219f2e0a1f6c815a4f0621140835
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-01-18 21:55:19 +0100

    more added

---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./Home.scss";
+
+const Home = () => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
+  );
+};
+
+export default Home;
